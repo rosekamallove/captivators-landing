259853379e5a93d3fc245e3584df80b1458db321
yash kumar mishra <mishrayash3778@gmail.com>
2022-09-01 22:37:06 +0530

    footermodified

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -29,8 +29,8 @@ export default function Home() {
         <PrimaryFeatures />
         {/* <SecondaryFeatures /> */}
         <CallToAction />
-        <Testimonials />
-        <Pricing />
+        {/* <Testimonials /> */}
+        {/* <Pricing /> */}
         {/* <Faqs /> */}
       </main>
       <Footer />
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,73 +4,66 @@ import Link from 'next/link'
 import { AuthLayout } from '@/components/AuthLayout'
 import { Button } from '@/components/Button'
 import { TextField } from '@/components/Fields'
+import InchargeImage from '@/images/Incharge.jpeg'
+import Image from 'next/image'
 import { Logo } from '@/components/Logo'
 import logo from '@/images/logos/Logo.jpeg'
-import Image from 'next/image'
+
 
 export default function Login() {
   return (
     <>
       <Head>
-        <title>Chairpersone</title>
+        <title>Student Incharge</title>
       </Head>
       <AuthLayout>
         <div className="flex flex-col">
-          <Link href="/" aria-label="Home">
-            <Image
-              className="h-14 w-14 object-cover"
-              src={logo}
-              alt=""
-              width={56}
-              height={56}
-            />
+        <Link href="/" aria-label="Home">
+          <Image
+                            className="h-14 w-14 object-cover"
+                            src={logo}
+                            alt=""
+                            width={56}
+                            height={56}
+                          />
           </Link>
-          <div className="mt-20">
-            <h2 className="text-lg font-semibold text-gray-900">
-              Sign in to your account
+          <div className="mt-10">
+          
+          <div className='flex justify-center w-100vw'>
+      <Image
+        className="w-full rounded-full mt-10"
+        src={InchargeImage}
+        alt=""
+        width={196}
+        height={196}
+      />
+    </div>
+    
+            <h2 className="text-xl mt-4  text-center font-semibold text-gray-900">
+            Harshit kumar singh
             </h2>
-            <p className="mt-2 text-sm text-gray-700">
-              Don’t have an account?{' '}
-              <Link
-                href="/register"
-                className="font-medium text-blue-600 hover:underline"
-              >
-                Sign up
-              </Link>{' '}
-              for a free trial.
-            </p>
+            <h2 className=" mt-4 text-sm text-center font-semibold text-gray-900">
+            Student Incharge
+            </h2>
+            <h2 className=" mt-4 text-sm text-center font-semibold text-gray-900">
+            Phone :- +91-8534833926
+            </h2>
+            <h2 className=" mt-4 text-sm text-center font-semibold text-gray-900">
+            Email :- club.csitmjpru@gmail.com
+            </h2>
+            <h2 className=" mt-4 text-sm text-center font-semibold text-gray-900">
+            Address :- Department of computer science and information technology , Mahatma Jyotiba Phule Rohilkhand University ,
+
+
+            </h2>
+            <h2 className=" mt-1 text-sm text-center font-semibold text-gray-900">
+          
+Bareilly , Uttar Pradesh , Pincode :- 243006
+
+            </h2>
           </div>
         </div>
-        <form action="#" className="mt-10 grid grid-cols-1 gap-y-8">
-          <TextField
-            label="Email address"
-            id="email"
-            name="email"
-            type="email"
-            autoComplete="email"
-            required
-          />
-          <TextField
-            label="Password"
-            id="password"
-            name="password"
-            type="password"
-            autoComplete="current-password"
-            required
-          />
-          <div>
-            <Button
-              type="submit"
-              variant="solid"
-              color="blue"
-              className="w-full"
-            >
-              <span>
-                Sign in <span aria-hidden="true">&rarr;</span>
-              </span>
-            </Button>
-          </div>
-        </form>
+        
       </AuthLayout>
     </>
   )
